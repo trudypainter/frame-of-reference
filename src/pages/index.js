c39d1cba69c490188512905d7f9e0561aad12cbf
trudypainter <35875567+trudypainter@users.noreply.github.com>
2023-07-07 13:29:36 -0400

    scaffolding for the question prompts. the subimt is not working yet

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,10 @@ import TerminalSimulator from "@/components/Terminal";
 import { useState } from "react";
 import SelectOption from "@/components/SelectOption";
 import Product from "@/components/Product";
+import Service from "@/components/Service";
+import API from "@/components/API";
+import Invention from "@/components/Invention";
+import Solution from "@/components/Solution";
 
 export default function Home() {
   // use state to keep track of the step of the user journey
@@ -26,6 +30,10 @@ export default function Home() {
             <SelectOption step={step} setStep={setStep} />
           )}
           {step === 2 && <Product step={step} setStep={setStep} />}
+          {step === 3 && <Service step={step} setStep={setStep} />}
+          {step === 4 && <API step={step} setStep={setStep} />}
+          {step === 5 && <Invention step={step} setStep={setStep} />}
+          {step === 6 && <Solution step={step} setStep={setStep} />}
         </div>
       </main>
     </>
